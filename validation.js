fce9c8f14042af32ce050a844b71aebfd16e4fff
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-08-19 23:19:44 -0500

    Added the classname input-type

--- validation.js
+++ validation.js
@@ -1,6 +1,6 @@
 let pattern = {
   username: /^[a-z\d]{5,12}$/i,
-  email: /[a-z\d.]+@[a-z\d]+.([a-z]+)(\.[a-z]+)*/i,
+  email: /([a-z\d.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?/,
   password: /[\w@-]{8,20}/,
   telephone: /^\d{11}$/g,
   slug: /[a-z\d-]{8,20}/i
@@ -9,9 +9,9 @@ let pattern = {
 //validate the regex
 function validate(field, regex) {
   if (regex.test(field.value)) {
-    field.className = "valid";
+    field.className = "input-type valid";
   } else {
-    field.className = "invalid";
+    field.className = "input-type invalid";
   }
 }
 
